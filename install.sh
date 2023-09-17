abb0673e708de8bce856bce3a8dd5f769aee9616
Martin Klapacz <klapacz.martin@gmail.com>
2023-09-17 22:29:43 +0200

    latex install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt -y install pdflatex
+sudo apt -y install texlive-latex-base
+sudo apt -y install texlive-fonts-recommended
+sudo apt -y install texlive-fonts-extra
+sudo apt -y install lstautogobble
+sudo apt -y install texlive-latex-extra
+sudo apt -y install texlive-lang-german
+sudo apt install texlive-bibtex-extra biber
